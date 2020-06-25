f145cd09a62113b73ad12e92df31cb40c3295cef
Mike Bernard <michael.bernard@uconn.edu>
2020-06-25 00:35:13 -0400

    create commander.py

---
+++ code/commander.py
@@ -0,0 +1,9 @@
+'''
+Filename: commander.py
+Programmed by: Mike Bernard
+Date: 2020-06-25
+
+Description: The main file that controls event sequences
+and delegates control to submodules for different phases
+of the mission.
+'''
