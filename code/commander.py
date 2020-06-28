161088b04be65490dbafacec89e78cbe67ab2495
Mike Bernard <michael.bernard@uconn.edu>
2020-06-27 23:25:24 -0400

    create commander.py

--- code/commander.py
+++ code/commander.py
@@ -7,3 +7,40 @@ Description: The main file that controls event sequences
 and delegates control to submodules for different phases
 of the mission.
 '''
+
+
+def main():
+    ''' This function commands the entire mission. '''
+    # mission setup
+
+    # Kerbin ascent
+
+    # Trans-Lunar Injection
+
+    # Orion-Gateway RPOD
+
+    # HLS-Gateway separation
+
+    # HLS deorbit
+
+    # HLS landing
+
+    # HLS ascent and tug rendezvous
+
+    # HLS-Tug RPOD
+
+    # HLS-Gateway intercept
+
+    # HLS-Gateway RPOD
+
+    # Orion-Gateway separation
+
+    # Trans-Kerbin Injection
+
+    # Kerbin re-entry and splashdown
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
