7542ac516dd90bedf29bb66604fbde7d15676d55
Mike Bernard <michael.bernard@uconn.edu>
2020-06-27 23:58:49 -0400

    add mission constants

--- code/util/commander/constants.py
+++ code/util/commander/constants.py
@@ -6,3 +6,11 @@ Date: 2020-06-25
 Description: Useful astrodynamics constants (some specific
 to Kerbal Space Program).
 '''
+
+
+# celestial body masses
+mass_kerbin = 5.2915158e22  # (kg)
+mass_mun = 9.7599066e20     # (kg)
+
+# astrodynamics constants
+G = 6.67408e-11  # (m**3/kg/s**2) gravitational constant
