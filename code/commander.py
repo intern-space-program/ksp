f993ad831073c88f62c825f1b9ee98b04c87d728
Mike Bernard <michael.bernard@uconn.edu>
2020-06-27 23:53:35 -0400

    create param.py

--- code/commander.py
+++ code/commander.py
@@ -9,9 +9,23 @@ of the mission.
 '''
 
 
+import krpc
+from code.param import Param
+from code.sequence_controllers import (
+    sc_00_COM_ES1_mission_setup,
+    sc_01_AE_ES1_kerbin_ascent,
+    sc_02_ORBIT_ES1_trans_lunar_injection,
+    sc_03_RPOD_ES1_orion_gateway_rpod,
+    sc_04_RPOD_ES2_hls_gateway_separation
+    # TODO: add more sequence controllers as they get defined
+)
+
+
 def main():
     ''' This function commands the entire mission. '''
     # mission setup
+    param = Param()
+    param = sc_00_COM_ES1_mission_setup.main(param)
 
     # Kerbin ascent
 
---
+++ code/param.py
@@ -0,0 +1,15 @@
+'''
+Filename: param.py
+Programmed by: Mike Bernard
+Date: 2020-06-27
+
+Description: One instance of the Param class will be
+created by commander.main. This instance will contain
+all important mission information, and will be passed
+to each event sequence's controlling function.
+'''
+
+
+class Param:
+    def __init__(self):
+        pass
