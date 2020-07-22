9fa8e117854ca754af774f4a1e013e409949b880
Mike Bernard <mike.bernard@uconn.edu>
2020-07-21 21:47:34 -0400

    create RPOD utils skeleton

---
+++ src/util/rpod/util.py
@@ -0,0 +1,27 @@
+'''
+Filename: util.py
+Programmed by: Mike Bernard
+Date: 2020-07-21
+
+Description: Utility functions for docking two vessels.
+'''
+
+
+def point_at_target(vessel, target):
+    ''' Orient the controlled vessel to point at the target vessel. '''
+    pass
+
+
+def get_to_target(vessel, target, final_dist):
+    ''' Burn toward the target and stop at a desired distance. '''
+    pass
+
+
+def translational_maneuver(vessel, target, safe_dist, final_pos, frame):
+    ''' Maneuver around a target to a desired position. '''
+    pass
+
+
+def dock(vessel, target_port):
+    ''' Dock to the target port assuming the two ports are already lined up. '''
+    pass
